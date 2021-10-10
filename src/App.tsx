d98f331fa8f66542d5853edaf7aa9cb5a8edcc99
NIDVELIR435 <mudryi435@gmail.com>
2021-10-10 20:06:04 +0300

    10.10.2021

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu} from 'antd';
+import { CheckSquareOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const { SubMenu } = Menu;
+  const { Header, Content, Sider, Footer } = Layout;
+
+  return <>
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu mode="inline" style={{ height: '100%', borderRight: 0 }}>
+              <Menu.Item key="1"> <CheckSquareOutlined /> Viev tasks </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+      <Footer style={{ textAlign: 'center' }}>©2021 Created by BandaPixels</Footer>
+    </Layout>
+
+  </>
 }
 
 export default App;
