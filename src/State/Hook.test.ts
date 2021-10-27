23af04d6dcbfc2d1a0b7eae4402d5709f0ce92ca
NIDVELIR435 <mudryi435@gmail.com>
2021-10-27 14:27:34 +0300

    27.10.2021

---
+++ src/State/Hook.test.ts
@@ -0,0 +1,7 @@
+import { useFilteredSelector } from "./Hooks";
+
+describe("Hook :", () => {
+  test("useFilteredSelector", () => {
+    const result = useFilteredSelector();
+  });
+});
--- src/State/Slices/TodoSlice.test.tsx
+++ src/State/Slices/TodoSlice.test.tsx
@@ -1,4 +1,3 @@
-import * as toolkit from "@reduxjs/toolkit";
 import {
   addTask,
   changeStatus,
@@ -7,40 +6,60 @@ import {
   sliceState,
   Status,
   todoReducer,
-} from "../TodoSlice";
-import { nanoid } from "@reduxjs/toolkit";
+  searchTask,
+} from "./TodoSlice";
+
+jest.mock("@reduxjs/toolkit", () => {
+  const mockedId = "MOCKED_ID";
+
+  // @ts-expect-error: mockValues is new prop
+  global.mockValues = {
+    mockedNanoidId: mockedId,
+  };
+  return {
+    ...jest.requireActual("@reduxjs/toolkit"),
+    nanoid: () => mockedId,
+  };
+});
 
 describe("TodoSlice actions most: ", () => {
-  let initState: sliceState = {
-    todo: [
-      {
-        id: "1id",
-        title: "1",
-        description: "1 description",
-        status: Status.Active,
-      },
-      {
-        id: "2id",
-        title: "2 title",
-        description: "2 description",
-        status: Status.Active,
-      },
-      {
-        id: "3id",
-        title: "3 title",
-        description: "3 description",
-        status: Status.Completed,
-      },
-      {
-        id: "4id",
-        title: "4 title",
-        description: "4 description",
-        status: Status.Completed,
-      },
-    ],
-    currentFilterValue: "",
-  };
+  let initState: sliceState;
+  let nanoidSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    initState = {
+      todo: [
+        {
+          id: "1id",
+          title: "1",
+          description: "1 description",
+          status: Status.Active,
+        },
+        {
+          id: "2id",
+          title: "2 title",
+          description: "2 description",
+          status: Status.Active,
+        },
+        {
+          id: "3id",
+          title: "3 title",
+          description: "3 description",
+          status: Status.Completed,
+        },
+        {
+          id: "4id",
+          title: "4 title",
+          description: "4 description",
+          status: Status.Completed,
+        },
+      ],
+      currentFilterValue: "",
+    };
+  });
   afterAll(() => {
+    // @ts-expect-error: mockValues is new prop
+    global.mockValues = {};
     initState = {
       todo: [
         {
@@ -70,33 +89,35 @@ describe("TodoSlice actions most: ", () => {
       ],
       currentFilterValue: "",
     };
+    nanoidSpy.mockRestore();
   });
   test("addTask-action added task", () => {
     let reducedInitState = todoReducer(
       initState,
       addTask({ title: "title", description: "description" })
     );
-
+    expect(reducedInitState.todo[0].id).toStrictEqual(
+      global.mockValues.mockedNanoidId
+    );
     expect(reducedInitState.todo.length).toBe(5);
   });
   test("addTask-action used mocked nanoid", () => {
-    let mockNanoid = jest
-      .spyOn(toolkit, "nanoid")
-      .mockReturnValue("nanoidValue");
-
     const reducedInitState = todoReducer(
       initState,
       addTask({ title: "title", description: "description" })
     );
-    expect(mockNanoid).toBeCalledTimes(1);
+    expect(reducedInitState.todo.length).toBe(5);
+    expect(reducedInitState.todo[0].id).toStrictEqual(
+      global.mockValues.mockedNanoidId
+    );
   });
   test("removeTask-action not delete task because unCorrect id", () => {
     let reducedInitState = todoReducer(initState, removeTask("unCorrect id"));
-    expect(reducedInitState.todo.length).toBe(3);
+    expect(reducedInitState.todo.length).toBe(4);
   });
   test("removeTask-action delete task", () => {
     let reducedInitState = todoReducer(initState, removeTask("1id"));
-    expect(reducedInitState.todo.length).toBe(2);
+    expect(reducedInitState.todo.length).toBe(3);
   });
   test("changeStatus-action upgrade ", () => {
     let reducedInitState = todoReducer(
@@ -117,4 +138,5 @@ describe("TodoSlice actions most: ", () => {
     let reducedInitState = todoReducer(initState, currentFilter(""));
     expect(reducedInitState.currentFilterValue).toBe("");
   });
+  test("searchTask-action return correct value+");
 });
